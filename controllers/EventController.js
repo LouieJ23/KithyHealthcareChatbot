aee204d8170e9702f50d2dda69cfadcb132576a7
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-22 07:18:41 +0800

    update

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -3,9 +3,15 @@
 const mongoose = require('mongoose');
 const Event = require('../models/Event');
 
+function SampleEvent(req, res) {
+    Event.findOne({}, function(err, events){
+        console.log(events);
+    })
+}
 exports.processRequests = (req,res) => {
-    res.json({
-        "fulfillmentText": "Sample Fulfillment",
-        "outputContexts": []
-    });
+    // res.json({
+    //     "fulfillmentText": "Sample Fulfillment",
+    //     "outputContexts": []
+    // });
+    SampleEvent(req, res);
 };
