356c9d63178288ab396bc5610d323dfe29a14979
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-30 04:07:01 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -16,7 +16,7 @@ function SampleEvent(req, res) {
     // console.log(location);
     
     if(location == "location"){
-        Event.findOne({eventLocation:location}, function (err, events) {
+        Event.findOne({eventLocation:"Kitaotao"}, function (err, events) {
             res.json({
                 "fulfillmentText": events.eventLocation,
                 "outputContexts": []
