a1d6da87eba0605845b9da2a9b1c658f378bca15
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-30 11:43:18 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -17,13 +17,13 @@ function SampleEvent(req, res) {
     // console.log(location);
     
     if(location == "location"){
-        Event.find({}, {eventLocation:"Kitaotao"}), function(err, events) {
+        Event.find({eventLocation:"Kitaotao"}).toArray(function(err, events) {
             res.json({
                 "fulfillmentText": events.eventLocation,
                 "outputContexts": []
             });
             console.log(events);
-        };
+        });
     };
    
 }
