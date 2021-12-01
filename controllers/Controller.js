f28a0bf99dfbd0ceb32c7271799e2f29903b5855
LouieJ23 <louiejaysalvana@gmail.com>
2021-12-01 13:25:44 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -15,71 +15,29 @@ function SampleEvent(req, res) {
     let event = req.body.queryResult.parameters.event;
     const input = req.body.queryResult.queryText;
     // console.log(location);
-
-    Event.findOne({ eventLocation: { $regex: /Location/ }}, function (err, events) {
-        var result = "The " + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
-        // console.log(events);
-        res.json({
-            "fulfillmentText": result,
-            "outputContexts": []
-        });
-    }).sort({ datePosted: 1 });
+    if(event == "latest") {
+        Event.findOne({}, function(err, events) {
+            var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
+            res.json({
+                "fulfillmentText": result,
+                "outputContexts": []
+            });
+        }).sort({datePosted:-1});
+    }
+    else {
+        
+        Event.findOne({ eventLocation: { $regex: /Location/ }}, function (err, events) {
+            var result = "The " + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
+            // console.log(events);
+            res.json({
+                "fulfillmentText": result,
+                "outputContexts": []
+            });
+        }).sort({ datePosted: 1 });
+    }
 }
 
-    function SampleDepartment(req, res) {
-        Department.findOne({}, function (err, department) {
-            res.json({
-                "fulfillmentText": department.depName,
-                "outputContexts": []
-            });
-        })
-    }
 
-    function SampleStaffInfo(req, res) {
-        StaffInfo.findOne({}, function (err, staffInfo) {
-            res.json({
-                "fulfillmentText": staffInfo.name,
-                "outputContexts": []
-            });
-        })
-    }
-
-    function SampleGuidelines(req, res) {
-        Guidelines.findOne({}, function (err, guidelines) {
-            res.json({
-                "fulfillmentText": guidelines.title,
-                "outputContexts": []
-            });
-        })
-    }
-
-    function SampleHcenterInfo(req, res) {
-        HcenterInfo.findOne({}, function (err, centerInfo) {
-            res.json({
-                "fulfillmentText": centerInfo.builtDate,
-                "outputContexts": []
-            });
-        })
-    }
-
-
-    function SampleMildIllness(req, res) {
-        MildIllness.findOne({}, function (err, illness) {
-            res.json({
-                "fulfillmentText": illness.title,
-                "outputContexts": []
-            });
-        })
-    }
-
-    function SampleAppointment(req, res) {
-        Appointment.findOne({}, function (err, appointment) {
-            res.json({
-                "fulfillmentText": appointment.category,
-                "outputContexts": []
-            });
-        })
-    }
 
     exports.processRequests = (req, res) => {
         if (req.body.queryResult.intent.displayName == "Events") {
--- controllers/sampleQuery.js
+++ controllers/sampleQuery.js
@@ -8,7 +8,8 @@ mongoose.connect("mongodb+srv://admin:user1@cluster0.a4dgc.mongodb.net/KithyDB",
     console.log("Connected to DB!");
 });
 
-Event.findOne({eventLocation: {$regex: /Location/}}, function(err, events) {
+var samp = "Location";
+Event.findOne({}, function(err, events) {
     var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
-    console.log(events);
-}).sort({datePosted:1});
+    console.log(result);
+}).sort({datePosted:-1});
