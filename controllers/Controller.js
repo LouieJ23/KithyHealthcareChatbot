0249e031b469d3e6ba43c1e223c21355c507730d
LouieJ23 <louiejaysalvana@gmail.com>
2021-12-07 02:05:44 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -13,9 +13,19 @@ const Admin = require('../routes/admin');
 
 function SampleEvent(req, res) {
     let event = req.body.queryResult.parameters.event;
+    let suggest = req.body.queryResult.queryText;
     const input = req.body.queryResult.queryText;
     // console.log(location);
     if(event == "latest") {
+        Event.findOne({}, function(err, events) {
+            var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
+            res.json({
+                "fulfillmentText": result,
+                "outputContexts": []
+            });
+        }).sort({datePosted:-1});
+    }
+    if(suggest == "Events"){
         Event.findOne({}, function(err, events) {
             var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
             res.json({
