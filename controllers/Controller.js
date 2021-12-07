ed18ec246275dc25da85bba2a0ede60179a1f6c9
LouieJ23 <louiejaysalvana@gmail.com>
2021-12-08 02:51:45 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -14,8 +14,8 @@ const Admin = require('../routes/admin');
 function SampleEvent(req, res) {
     let event = req.body.queryResult.parameters.event;
     let suggest = req.body.queryResult.queryText;
-    const input = req.body.queryResult.queryText;
     // console.log(location);
+
     if(event == "latest") {
         Event.findOne({}, function(err, events) {
             var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
@@ -25,7 +25,7 @@ function SampleEvent(req, res) {
             });
         }).sort({datePosted:-1});
     }
-    if(suggest == "Events"){
+    if(suggest == "Event"){
         Event.findOne({}, function(err, events) {
             var result = "The "+events.eventTitle+" will be going to held  in "+events.eventLocation+". So in order to participate to the event, you are required to bring "+events.eventRequire+". The process is to "+events.eventProcess+" and the participants are "+events.eventParticipants;
             res.json({
@@ -37,7 +37,7 @@ function SampleEvent(req, res) {
     else {
         
         Event.findOne({ eventLocation: { $regex: /Location/ }}, function (err, events) {
-            var result = "The previous event" + events.eventTitle + " was held in " + events.eventLocation + ". The requirement for this event is/are: " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
+            var result = "The previous event is " + events.eventTitle + " was held in " + events.eventLocation + ". The requirement for this event is/are: " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
             // console.log(events);
             res.json({
                 "fulfillmentText": result,
