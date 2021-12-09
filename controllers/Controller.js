d30a1b187ebfc580b6f07da1fd14a4eb74950f5a
LouieJ23 <louiejaysalvana@gmail.com>
2021-12-10 01:57:26 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -25,7 +25,7 @@ function SampleEvent(req, res) {
             });
         }).sort({ datePosted: -1 });
     }
-    
+
     if (suggest == "Event Name") {
         Event.findOne({}, function (err, events) {
             var result = "The Event Name" + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
@@ -36,18 +36,17 @@ function SampleEvent(req, res) {
         }).sort({ datePosted: -1 });
     }
 
-    else {
-
-    Event.findOne({ eventLocation: { $regex: /Location/ }}, function (err, events) {
-        var result = "The previous event is " + events.eventTitle + " was held in " + events.eventLocation + ". The requirement for this event is/are: " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
-        // console.log(events);
-        res.json({
-            "fulfillmentText": result,
-            "outputContexts": []
-        });
-    }).sort({ datePosted: 1 });
-}
-// console.log(suggest);
+    // else {
+    //     Event.findOne({ eventLocation: { $regex: /Location/ } }, function (err, events) {
+    //         var result = "The previous event is " + events.eventTitle + " was held in " + events.eventLocation + ". The requirement for this event is/are: " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
+    //         // console.log(events);
+    //         res.json({
+    //             "fulfillmentText": result,
+    //             "outputContexts": []
+    //         });
+    //     }).sort({ datePosted: 1 });
+    // }
+    // console.log(suggest);
 }
 //     if (event == "previous") {
 //         Event.findOne({}, function (err, events) {
