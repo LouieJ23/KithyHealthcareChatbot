1255196507e80f428f354da691e70dbfe093fa50
LouieJ23 <louiejaysalvana@gmail.com>
2021-12-10 02:53:34 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -12,12 +12,12 @@ const Admin = require('../routes/admin');
 
 
 function SampleEvent(req, res) {
-    let event = req.body.queryResult.parameters.event;
+    let event = req.body.queryResult.parameters.event.latest;
     let suggest = req.body.queryResult.queryText;
    
     // console.log(location);
 
-    if (suggest == "Event Name") {
+    if (suggest == event) {
         Event.findOne({}, function (err, events) {
             var result = "The " + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
             res.json({
@@ -27,15 +27,15 @@ function SampleEvent(req, res) {
         }).sort({ datePosted: -1 });
     }
 
-    // if (suggest == "Event Name") {
-    //     Event.findOne({}, function (err, events) {
-    //         var result = "The Event Name" + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
-    //         res.json({
-    //             "fulfillmentText": result,
-    //             "outputContexts": []
-    //         });
-    //     }).sort({ datePosted: -1 });
-    // }
+    if (suggest == "Event Name") {
+        Event.findOne({}, function (err, events) {
+            var result = "The Event Name" + events.eventTitle + " will be going to held  in " + events.eventLocation + ". So in order to participate to the event, you are required to bring " + events.eventRequire + ". The process is to " + events.eventProcess + " and the participants are " + events.eventParticipants;
+            res.json({
+                "fulfillmentText": result,
+                "outputContexts": []
+            });
+        }).sort({ datePosted: -1 });
+    }
 }
 
     // else {
