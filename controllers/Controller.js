ee6eeabd27dac4566e0a4c396a3d5f6bbcdf4133
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-16 16:33:04 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -19,10 +19,26 @@ function _Event(req, res) {
             Event.find({}, function (err, events) {
                 const event = events[0];
                 var result = "The " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
+                // res.json({
+                //     "fulfillmentText": result,
+                //     "outputContexts": []
+                // });
+
                 res.json({
-                    "fulfillmentText": result,
-                    "outputContexts": []
-                });
+                    "fulfillment_messages": [{
+                      "payload": {
+                        "richContent": [
+                          [{
+                            "type": "chips",
+                            "options": [{
+                              "text": "Empezar!"
+                            }]
+                          }]
+                        ]
+                      }
+                    }]
+                  
+                  });
             }).sort({ datePosted: -1 });
     }
     else if (suggest === 'previous') {
