defb5d93c918fe28327ef8f7fcf08b91a3043222
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-17 10:42:08 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -13,9 +13,11 @@ const Admin = require('../routes/admin');
 
 function _Event(req, res) {
     let intent_name = req.body.queryResult.intent.displayName;
-    
+    let events = await Event.find().sort({ datePosted: -1 });
+    let eventDate = events[0].datePosted;
+    let dateToday = Date.now();
     if (intent_name == 'Events') {
-        Event.find({}, function (err, events) {
+        
             const event = events[0];
             var result = "The latest event name is " + event.eventTitle;
             res.json({
@@ -39,87 +41,88 @@ function _Event(req, res) {
                         }
                     }
                 ]
-            });
-        });
-        
+            });        
     }
-    else if (intent_name == 'Events - latest') {
-            Event.find({}, function (err, events) {
-                const event = events[0];
-                var result = "The " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
-                
-                
-                res.json({
-                    "fulfillmentMessages": [
-                        {
-                            "quickReplies": {
-                                "title": result,
-                                "quickReplies": [
-                                    "Event",
-                                    "Health Center",
-                                    "Illness",
-                                    "Set Appointment",
-                                    "Visit Site"
-                                ]
-                            },
-                            "platform": "FACEBOOK"
-                        },
-                        {
-                            "text": {
-                                "text": [
-                                    ""
-                                ]
-                            }
-                        }
-                    ]
-                });
-            }).sort({ datePosted: -1 });
-        }
-        else if (intent_name == 'Events - previous') {
-                Event.find({}, function (err, events) {
-                    const event = events[1];
-                    var result = "The recent event is " + event.eventTitle + " was held at " + event.eventLocation + ". The participants were required to  " + event.eventRequire + ". The process is:  " + event.eventProcess + " and the participants are " + event.eventParticipant;
-                    res.json({
-                        "fulfillmentMessages": [
-                            {
-                                "quickReplies": {
-                                    "title": "What would you like to know about Event?",
-                                    "quickReplies": [
-                                        "Upcoming",
-                                        "Latest",
-                                        "Previous"
-                                    ]
-                                },
-                                "platform": "FACEBOOK"
-                            },
-                            {
-                                "text": {
-                                    "text": [
-                                        result
-                                    ]
-                                }
-                            }
-                        ]
-                    });
-                }).sort({ datePosted: -1 });
-            }
     else {
-        res.json({
-            "fulfillmentText": req.body.queryResult.fulfillmentMessages.text.text[0],
-            "outputContexts": []
-        });
+        if(dateToday > eventDate) {
+            console.log("Latest Date");
+        }
+        else {
+            console.log("Upcoming Date");
+        }
     }
+    // else if (intent_name == 'Events - latest') {
+    //         Event.find({}, function (err, events) {
+    //             const event = events[0];
+    //             var result = "The " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
+                
+                
+    //             res.json({
+    //                 "fulfillmentMessages": [
+    //                     {
+    //                         "quickReplies": {
+    //                             "title": result,
+    //                             "quickReplies": [
+    //                                 "Event",
+    //                                 "Health Center",
+    //                                 "Illness",
+    //                                 "Set Appointment",
+    //                                 "Visit Site"
+    //                             ]
+    //                         },
+    //                         "platform": "FACEBOOK"
+    //                     },
+    //                     {
+    //                         "text": {
+    //                             "text": [
+    //                                 ""
+    //                             ]
+    //                         }
+    //                     }
+    //                 ]
+    //             });
+    //         }).sort({ datePosted: -1 });
+    //     }
+    //     else if (intent_name == 'Events - previous') {
+    //             Event.find({}, function (err, events) {
+    //                 const event = events[1];
+    //                 var result = "The recent event is " + event.eventTitle + " was held at " + event.eventLocation + ". The participants were required to  " + event.eventRequire + ". The process is:  " + event.eventProcess + " and the participants are " + event.eventParticipant;
+    //                 res.json({
+    //                     "fulfillmentMessages": [
+    //                         {
+    //                             "quickReplies": {
+    //                                 "title": "What would you like to know about Event?",
+    //                                 "quickReplies": [
+    //                                     "Upcoming",
+    //                                     "Latest",
+    //                                     "Previous"
+    //                                 ]
+    //                             },
+    //                             "platform": "FACEBOOK"
+    //                         },
+    //                         {
+    //                             "text": {
+    //                                 "text": [
+    //                                     result
+    //                                 ]
+    //                             }
+    //                         }
+    //                     ]
+    //                 });
+    //             }).sort({ datePosted: -1 });
+    //         }
+    // else {
+    //     res.json({
+    //         "fulfillmentText": req.body.queryResult.fulfillmentMessages.text.text[0],
+    //         "outputContexts": []
+    //     });
+    // }
 
 }
 
 
 exports.processRequests = (req, res) => {
 
-    let intent_name = req.body.queryResult.intent.displayName;
-    if (intent_name == 'Events') {
-        _Event(req, res);
-    }
-    
-
+    _Event(req, res);
 };
 
