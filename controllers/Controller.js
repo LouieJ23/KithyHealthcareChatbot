4573bbcb2f6e547fbb8a806d3b3f2aaca1022fe5
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-17 10:06:41 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -12,13 +12,72 @@ const Admin = require('../routes/admin');
 
 
 async function _Event(req, res) {
-    const event = await Event.findOne().sort({ datePosted: -1 });
-
+    let event = await Event.findOne().sort({ datePosted: -1 });
     let intent_name = req.body.queryResult.intent.displayName;
-    
+    let eventDate = event.datePosted;
+    let dateToday = Date.now();
+
     if (intent_name == 'Events') {
-       
-            var result = "The latest event name is " + event.eventTitle;
+
+        var result = "The latest event name is " + event.eventTitle;
+        res.json({
+            "fulfillmentMessages": [
+                {
+                    "quickReplies": {
+                        "title": "What would you like to know about Event?",
+                        "quickReplies": [
+                            "Upcoming",
+                            "Latest",
+                            "Previous"
+                        ]
+                    },
+                    "platform": "FACEBOOK"
+                },
+                {
+                    "text": {
+                        "text": [
+                            result
+                        ]
+                    }
+                }
+            ]
+        });
+    }
+
+    let events = await Event.findOne().sort({ datePosted: -1 });
+    if (datePosted > eventDate) {
+        if (intent_name === 'Events - latest') {
+            let event = events[0];
+            var result = "The latest " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
+
+            res.json({
+                "fulfillmentMessages": [
+                    {
+                        "quickReplies": {
+                            "title": result,
+                            "quickReplies": [
+                                "Event",
+                                "Health Center",
+                                "Illness",
+                                "Set Appointment",
+                                "Visit Site"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                    {
+                        "text": {
+                            "text": [
+                                ""
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
+        else if (intent_name === 'Events - previous') {
+            let event = events[1];
+            var result = "The previous event is " + event.eventTitle + " was held at " + event.eventLocation + ". The participants were required to  " + event.eventRequire + ". The process is:  " + event.eventProcess + " and the participants are " + event.eventParticipant;
             res.json({
                 "fulfillmentMessages": [
                     {
@@ -41,90 +100,54 @@ async function _Event(req, res) {
                     }
                 ]
             });
+        }
+        else {
+            res.json({
+                "fulfillmentText": req.body.queryResult.fulfillmentMessages.text.text[0],
+                "outputContexts": []
+            });
+        }
     }
-    
-    // else if (intent_name === 'Events - latest') {
-    //         Event.find({}, function (err, events) {
-    //             const event = events[0];
-    //             let eventDate = event.datePosted;
-    //             let dateToday = Date.now();
-    //             var result = "The " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
+    else {
+        if (intent_name === 'Events - upcoming') {
+            let event = events[0];
+            var result = "The upcoming" + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
 
-    //             if(dateToday > eventDate) {
-
-    //             }
-
-    //             res.json({
-    //                 "fulfillmentMessages": [
-    //                     {
-    //                         "quickReplies": {
-    //                             "title": result,
-    //                             "quickReplies": [
-    //                                 "Event",
-    //                                 "Health Center",
-    //                                 "Illness",
-    //                                 "Set Appointment",
-    //                                 "Visit Site"
-    //                             ]
-    //                         },
-    //                         "platform": "FACEBOOK"
-    //                     },
-    //                     {
-    //                         "text": {
-    //                             "text": [
-    //                                 ""
-    //                             ]
-    //                         }
-    //                     }
-    //                 ]
-    //             });
-    //         }).sort({ datePosted: -1 });
-    //     }
-    //     else if (intent_name === 'Events - previous') {
-    //             Event.find({}, function (err, events) {
-    //                 const event = events[1];
-    //                 var result = "The recent event is " + event.eventTitle + " was held at " + event.eventLocation + ". The participants were required to  " + event.eventRequire + ". The process is:  " + event.eventProcess + " and the participants are " + event.eventParticipant;
-    //                 res.json({
-    //                     "fulfillmentMessages": [
-    //                         {
-    //                             "quickReplies": {
-    //                                 "title": "What would you like to know about Event?",
-    //                                 "quickReplies": [
-    //                                     "Upcoming",
-    //                                     "Latest",
-    //                                     "Previous"
-    //                                 ]
-    //                             },
-    //                             "platform": "FACEBOOK"
-    //                         },
-    //                         {
-    //                             "text": {
-    //                                 "text": [
-    //                                     result
-    //                                 ]
-    //                             }
-    //                         }
-    //                     ]
-    //                 });
-    //             }).sort({ datePosted: -1 });
-    //         }
-    // else {
-    //     res.json({
-    //         "fulfillmentText": req.body.queryResult.fulfillmentMessages.text.text[0],
-    //         "outputContexts": []
-    //     });
-    // }
-
-}
-
-
-exports.processRequests = (req, res) => {
-
-    let intent_name = req.body.queryResult.intent.displayName;
-    if (intent_name == 'Events') {
-        _Event(req, res);
+            res.json({
+                "fulfillmentMessages": [
+                    {
+                        "quickReplies": {
+                            "title": result,
+                            "quickReplies": [
+                                "Event",
+                                "Health Center",
+                                "Illness",
+                                "Set Appointment",
+                                "Visit Site"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                    {
+                        "text": {
+                            "text": [
+                                ""
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
     }
 
 
-};
+    exports.processRequests = (req, res) => {
+
+        let intent_name = req.body.queryResult.intent.displayName;
+        if (intent_name == 'Events') {
+            _Event(req, res);
+        }
+
+
+    };
 
