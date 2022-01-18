7570040a9e4d9dcb845e1a544edd6008eac30860
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-19 00:39:10 +0800

    new update

--- controllers/intents/HealthCenter.js
+++ controllers/intents/HealthCenter.js
@@ -6,11 +6,11 @@ const hCenter = require('../../models/CenterInfo');
 function _HealthCenter(req, res) {
     let intent_name = req.body.queryResult.intent.displayName;
     console.log
-    if (intent_name == "Date Of Founding") {
+    if (intent_name == "Health Center") {
         hCenter.find({}, function (err, centerInfos) {
             const centerInfo = centerInfos[0];
             var result = "The health center of Kitaotao is founded on  " + centerInfo.dateOfFounding;
-
+            
             res.json({
                 "fulfillmentMessages": [
                     {
@@ -40,36 +40,8 @@ function _HealthCenter(req, res) {
             });
         })
     }
-    else {
-        res.json({
-            "fulfillmentMessages": [
-                {
-                    "quickReplies": {
-                       
-                        "quickReplies": [
-                            "Back",
-                            "Date of Founding",
-                            "Published by",
-                            "Location",
-                            "Contact Number",
-                            "Email Address",
-                            "Mission",
-                            "Vision"
-                           
-                        ]
-                    },
-                    "platform": "FACEBOOK"
-                },
-                {
-                    "text": {
-                        "text": [
-                            ""
-                        ]
-                    }
-                }
-            ]
-        });
-    }
-    }
-
+   
+   
+    
+}
 module.exports = _HealthCenter;
