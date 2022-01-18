73c9e73e4dd2c3148954ca3b2617be6f37ba957c
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-19 02:51:30 +0800

    new update

--- controllers/intents/HealthCenter.js
+++ controllers/intents/HealthCenter.js
@@ -6,39 +6,39 @@ const hCenter = require('../../models/CenterInfo');
 function _HealthCenter(req, res) {
     let intent_name = req.body.queryResult.intent.displayName;
     // console.log(intent_name);
-    if (intent_name == "Health Center") {
-        // hCenter.findOne({}, function (err, centerInfo) {
-            // var result = "The health center of Kitaotao is founded on  " + centerInfo.dateOfFounding;
+    // if (intent_name == "Health Center") {
+    //     // hCenter.findOne({}, function (err, centerInfo) {
+    //         // var result = "The health center of Kitaotao is founded on  " + centerInfo.dateOfFounding;
             
-            res.json({
-                "fulfillmentMessages": [
-                    {
-                        "quickReplies": {
-                            "title": "",
-                            "quickReplies": [
-                                "Date of Founding",
-                                "Published by",
-                                "Location",
-                                "Contact Number",
-                                "Email Address",
-                                "Mission",
-                                "Vision"
+    //         res.json({
+    //             "fulfillmentMessages": [
+    //                 {
+    //                     "quickReplies": {
+    //                         "title": "",
+    //                         "quickReplies": [
+    //                             "Date of Founding",
+    //                             "Published by",
+    //                             "Location",
+    //                             "Contact Number",
+    //                             "Email Address",
+    //                             "Mission",
+    //                             "Vision"
                                
-                            ]
-                        },
-                        "platform": "FACEBOOK"
-                    },
-                    {
-                        "text": {
-                            "text": [
-                                ""
-                            ]
-                        }
-                    }
-                ]
-            });
-        // })
-    }
+    //                         ]
+    //                     },
+    //                     "platform": "FACEBOOK"
+    //                 },
+    //                 {
+    //                     "text": {
+    //                         "text": [
+    //                             ""
+    //                         ]
+    //                     }
+    //                 }
+    //             ]
+    //         });
+    //     // })
+    // }
    
    
     if (intent_name == "Health Center - date of founding") {
