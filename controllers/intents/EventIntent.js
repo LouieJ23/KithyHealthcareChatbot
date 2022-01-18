28af18b49ea6fccbd1dbb6da289ffb1d75de3987
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-18 21:46:34 +0800

    update

--- controllers/intents/EventIntent.js
+++ controllers/intents/EventIntent.js
@@ -246,7 +246,7 @@ function _Event(req, res) {
         }).sort({ datePosted: -1 });
     }
 
-     if (intent_name == 'latest') {
+     if (intent_name == 'Events - latest') {
         Event.find({}, function (err, events) {
             const event = events[0];
             var result = "The latest events " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
