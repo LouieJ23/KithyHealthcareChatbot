eedc807b0c8af2f23762c709aa6c1abf5954fd61
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-19 10:47:30 +0800

    update

--- controllers/intents/EventIntent.js
+++ controllers/intents/EventIntent.js
@@ -350,7 +350,7 @@ function _Event(req, res) {
             });
         }).sort({ datePosted: -1 });
     }
-    if (intent_name == intent_name == "Previous Event") {
+    if (intent_name == "Previous Event") {
         Event.find({}, function (err, events) {
             const event = events[1];
             var result = "The previous events " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant;
