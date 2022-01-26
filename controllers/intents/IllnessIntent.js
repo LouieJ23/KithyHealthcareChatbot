54f01d8315b676e99815860a0dc5afe3fb1efb6f
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-26 22:27:31 +0800

    update

--- controllers/intents/IllnessIntent.js
+++ controllers/intents/IllnessIntent.js
@@ -39,23 +39,20 @@ function _Illness(req, res) {
     }
     if (intent_name == 'Illness - more') {
         Illness.find({}, function (err, illness) {
-            const illnesses = [];
-            for(let i in illness){
-                illnesses.push(illness[i].title);
-            }
+            const value = req.body.queryResult.queryText;
 
             res.json({
                 "fulfillmentMessages": [
                     {
                         "quickReplies": {
-                            "title": "What would you like to know about " + req.body.queryResult.queryText + "?",
+                            "title": "What would you like to know about " + value + "?",
                             "quickReplies": [
-                                "Title",
-                                "Details",
-                                "Symptoms",
-                                "Treatment",
-                                "Prevention",
-                                "Go Back"
+                                value + "Title",
+                                value + "Details",
+                                value + "Symptoms",
+                                value + "Treatment",
+                                value + "Prevention",
+                                value + "Go Back"
                             ]
                         },
                         "platform": "FACEBOOK"
@@ -75,20 +72,23 @@ function _Illness(req, res) {
     }
     if (intent_name == 'Illness - more - Details') {
         Illness.find({}, function (err, illness) {
-            const value = req.body.queryResult.queryText;
+            const illnesses = [];
+            for(let i in illness){
+                illnesses.push(illness[i].detail);
+            }
 
             res.json({
                 "fulfillmentMessages": [
                     {
                         "quickReplies": {
-                            "title": "What would you like to know about " + value + "?",
+                            "title": "What would you like to know about " + req.body.queryResult.queryText + "?",
                             "quickReplies": [
-                                value + "Title",
-                                value + "Details",
-                                value + "Symptoms",
-                                value + "Treatment",
-                                value + "Prevention",
-                                value + "Go Back"
+                                "Title",
+                                "Details",
+                                "Symptoms",
+                                "Treatment",
+                                "Prevention",
+                                "Go Back"
                             ]
                         },
                         "platform": "FACEBOOK"
