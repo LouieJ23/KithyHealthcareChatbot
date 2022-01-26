3d38ea9ce12a188cd8857258adde78374171451e
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-26 20:59:23 +0800

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const _Event = require('./intents/EventIntent');
 const _HealthCenter = require('./intents/HealthCenter');
+const _Illness = require('./intents/IllnessAPI');
 const Department = require('../models/Departments');
 const StaffInfo = require('../models/Staffs');
 const Guidelines = require('../models/Guidelines');
@@ -14,4 +15,5 @@ const Admin = require('../routes/admin');
 exports.processRequests = (req, res) => {
     _Event(req, res);
     _HealthCenter(req, res);
+    _Illness(req, res);
 };
