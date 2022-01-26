2da5c3a1a591edfbcc8c49e5f41c3d11715d4c39
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-26 21:10:48 +0800

    update

--- controllers/intents/IllnessIntent.js
+++ controllers/intents/IllnessIntent.js
@@ -8,16 +8,17 @@ function _Illness(req, res) {
     console.log(intent_name);
     if (intent_name == 'Illness') {
         Illness.find({}, function (err, illness) {
+            const illnesses = [];
+            for(let i in illness){
+                illnesses.push(illness[i].title);
+            }
+
             res.json({
                 "fulfillmentMessages": [
                     {
                         "quickReplies": {
                             "title": "What would you like to know about Illness?",
-                            "quickReplies": [
-                                "suka",
-                                "sipon",
-                                "sakit tiyan"
-                            ]
+                            "quickReplies": illnesses
                         },
                         "platform": "FACEBOOK"
                     },
@@ -32,7 +33,7 @@ function _Illness(req, res) {
                 ]
             });
 
-            console.log(illness);
+            console.log(illnesses);
         });
 
     }
