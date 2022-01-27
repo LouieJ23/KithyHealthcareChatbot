ee4ab06ba5cd6710805a5f936685b029ba25031c
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-27 13:46:28 +0800

    update

--- controllers/intents/IllnessIntent.js
+++ controllers/intents/IllnessIntent.js
@@ -34,103 +34,78 @@ async function _Illness(req, res) {
                 }
             ]
         });
-        // Illness.find({}, function (err, illness) {
-            
-
-        //     res.json({
-        //         "fulfillmentMessages": [
-        //             {
-        //                 "quickReplies": {
-        //                     "title": "What would you like to know about Illness?",
-        //                     "quickReplies": illnesses
-        //                 },
-        //                 "platform": "FACEBOOK"
-        //             },
-        //             {
-        //                 "text": {
-        //                     "text": [
-        //                         ""
-        //                         // result
-        //                     ]
-        //                 }
-        //             }
-        //         ]
-        //     });
-
-        //     console.log(illnesses);
-        // });
 
         
     }
-    // if (intent_name == 'Illness - more') {
-    //     Illness.find({}, function (err, illness) {
-    //         const value = req.body.queryResult.queryText;
+    if (intent_name == 'Illness - more') {
+        Illness.find({}, function (err, illness) {
+            const value = req.body.queryResult.queryText;
 
-    //         res.json({
-    //             "fulfillmentMessages": [
-    //                 {
-    //                     "quickReplies": {
-    //                         "title": "What would you like to know about " + value + "?",
-    //                         "quickReplies": [
-    //                             value + " Title",
-    //                             value + " Details",
-    //                             value + " Symptoms",
-    //                             value + " Treatment",
-    //                             value + " Prevention",
-    //                             value + " Go Back"
-    //                         ]
-    //                     },
-    //                     "platform": "FACEBOOK"
-    //                 },
-    //                 {
-    //                     "text": {
-    //                         "text": [
-    //                             ""
-    //                             // result
-    //                         ]
-    //                     }
-    //                 }
-    //             ]
-    //         });
-    //     });
+            res.json({
+                "fulfillmentMessages": [
+                    {
+                        "quickReplies": {
+                            "title": "What would you like to know about " + value + "?",
+                            "quickReplies": [
+                                value + " Title",
+                                value + " Details",
+                                value + " Symptoms",
+                                value + " Treatment",
+                                value + " Prevention",
+                                value + " Go Back"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                    {
+                        "text": {
+                            "text": [
+                                ""
+                                // result
+                            ]
+                        }
+                    }
+                ]
+            });
+        });
 
-    // }
-    // if (intent_name == 'Illness - more - Details') {
-    //     Illness.find({title: "diarhhea" }, function (err, illness) {
-    //         const illnesses = [];
-    //         for(let i in illness){
-    //             illnesses.push(illness[i].detail);
-    //         }
+    }
+    if (intent_name == 'Illness - more - Details') {
+        Illness.find({title: "diarhhea" }, function (err, illness) {
+            const illnesses = [];
+            for(let i in illness){
+                illnesses.push(illness[i].detail);
+            }
 
-    //         res.json({
-    //             "fulfillmentMessages": [
-    //                 {
-    //                     "quickReplies": {
-    //                         "title": "What would you like to know about " + req.body.queryResult.queryText + "?",
-    //                         "quickReplies": [
-    //                             "Title",
-    //                             "Details",
-    //                             "Symptoms",
-    //                             "Treatment",
-    //                             "Prevention",
-    //                             "Go Back"
-    //                         ]
-    //                     },
-    //                     "platform": "FACEBOOK"
-    //                 },
-    //                 {
-    //                     "text": {
-    //                         "text": [
-    //                             ""
-    //                             // result
-    //                         ]
-    //                     }
-    //                 }
-    //             ]
-    //         });
-    //     });
+            res.json({
+                "fulfillmentMessages": [
+                    {
+                        "quickReplies": {
+                            "title": "What would you like to know about " + req.body.queryResult.queryText + "?",
+                            "quickReplies": [
+                                "Title",
+                                "Details",
+                                "Symptoms",
+                                "Treatment",
+                                "Prevention",
+                                "Go Back"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                    {
+                        "text": {
+                            "text": [
+                                ""
+                                // result
+                            ]
+                        }
+                    }
+                ]
+            });
+        });
 
-    // }
+    }
 }
 
 module.exports = _Illness;
