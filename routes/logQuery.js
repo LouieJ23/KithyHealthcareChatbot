51a278b7d2904df22fcbb441a2da17b5a7e9c79e
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-28 19:28:10 +0800

    update

---
+++ routes/logQuery.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const Log = require('../models/Logs');
+
+router.use((req, res, next) => {
+    if(req.query._method == 'DELETE') {
+        req.method = 'DELETE';
+        req.url = req.path
+    }
+
+if(req.query._method == 'PUT') {
+    req.method = 'PUT';
+    req.url = req.path
+}
+    next();
+});
+
+
+
+router.get('/', async (req, res) => {
+    try { 
+        const {page = 1, limit = 4} = req.query;
+        const log = await Log.find()
+        .sort({ datePosted: -1 })
+        .limit(limit * 1)
+       .skip((page - 1) * limit);
+        res.render('event', {
+            events: event,
+            page_name: 'log',
+            next: parseInt(page) + 1,
+            prev: parseInt(page) - 1,
+            isPaginate: true
+        })
+      
+        
+    }
+    catch(err) {
+        res.json({
+            message: err
+        });
+    }
+
+});
+
+
+module.exports = router;
