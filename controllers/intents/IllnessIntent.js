711cb4478160d9eaafa4b84496465fda19b66716
LouieJ23 <louiejaysalvana@gmail.com>
2022-01-29 12:06:16 +0800

    update

--- controllers/intents/IllnessIntent.js
+++ controllers/intents/IllnessIntent.js
@@ -6,18 +6,16 @@ const LogQuery = require('../../models/Logs');
 
 async function _Illness(req, res) {
     let intent_name = req.body.queryResult.intent.displayName;
-    const value = req.body.queryResult.queryText;
-
-    const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-    const query = recentLog.query;
-    console.log("Intent Name: " + intent_name);
-    console.log("Query Text: " + value);
-    console.log("Recent Log Query: " + query);
 
     if (intent_name == 'Illness') {
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        const query = recentLog.query;
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
         const illness = await Illness.find();
         const illnesses = [];
-        for(let i in illness){
+        for (let i in illness) {
             illnesses.push(illness[i].title);
         }
 
@@ -44,101 +42,101 @@ async function _Illness(req, res) {
             query: value,
             isAnswered: true,
         });
-        await log1.save();    
+        await log1.save();
     }
     else if (intent_name == 'Illness - more') {
-            const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-            // console.log(recentLog);
-            const value = req.body.queryResult.queryText;
-            const query = recentLog.query;
-            console.log("Intent Name: " + intent_name);
-            console.log("Query Text: " + value);
-            console.log("Recent Log Query: " + query);
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        // console.log(recentLog);
+        const value = req.body.queryResult.queryText;
+        const query = recentLog.query;
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
 
 
-            res.json({
-                "fulfillmentMessages": [
-                    {
-                        "quickReplies": {
-                            "title": "What would you like to know more about " + value + "?",
-                            "quickReplies": [
-                                " Details",
-                                " Symptoms",
-                                " Treatment",
-                                " Prevention",
-                                " Go Back"
-                            ]
-                        },
-                        "platform": "FACEBOOK"
+        res.json({
+            "fulfillmentMessages": [
+                {
+                    "quickReplies": {
+                        "title": "What would you like to know more about " + value + "?",
+                        "quickReplies": [
+                            " Details",
+                            " Symptoms",
+                            " Treatment",
+                            " Prevention",
+                            " Go Back"
+                        ]
                     },
-                    {
-                        "text": {
-                            "text": [
-                                ""
-                                // result
-                            ]
-                        }
+                    "platform": "FACEBOOK"
+                },
+                {
+                    "text": {
+                        "text": [
+                            ""
+                            // result
+                        ]
                     }
-                ]
-            });
+                }
+            ]
+        });
 
         const log2 = new LogQuery({
             query: value,
             isAnswered: true,
         });
-        await log2.save();   
+        await log2.save();
 
     }
     else if (intent_name == 'Illness - more - details') {
-            const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-            const value = req.body.queryResult.queryText;
-            const query = recentLog.query;
-            const illness = await Illness.find({title: query});
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        const value = req.body.queryResult.queryText;
+        const query = recentLog.query;
+        const illness = await Illness.find({ title: query });
 
-            console.log("Intent Name: " + intent_name);
-            console.log("Query Text: " + value);
-            console.log("Recent Log Query: " + query);
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
 
-            res.json({
-                "fulfillmentMessages": [
-                    {
-                        "quickReplies": {
-                            "title": illness[0].detail,
-                            "quickReplies": [
-                                "Details",
-                                "Symptoms",
-                                "Treatment",
-                                "Prevention",
-                                "Go Back"
-                            ]
-                        },
-                        "platform": "FACEBOOK"
+        res.json({
+            "fulfillmentMessages": [
+                {
+                    "quickReplies": {
+                        "title": illness[0].detail,
+                        "quickReplies": [
+                            "Details",
+                            "Symptoms",
+                            "Treatment",
+                            "Prevention",
+                            "Go Back"
+                        ]
                     },
-                    {
-                        "text": {
-                            "text": [
-                                ""
-                                // result
-                            ]
-                        }
+                    "platform": "FACEBOOK"
+                },
+                {
+                    "text": {
+                        "text": [
+                            ""
+                            // result
+                        ]
                     }
-                ]
-            });
-            const log3 = new LogQuery({
-                query: value,
-                isAnswered: false,
-            });
-            await log3.save();   
+                }
+            ]
+        });
+        const log3 = new LogQuery({
+            query: value,
+            isAnswered: false,
+        });
+        await log3.save();
     }
     else if (intent_name == 'Illness - more - symptoms') {
-        const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-            const value = req.body.queryResult.queryText;
-            const query = recentLog.query;
-            const illness = await Illness.find({title: query});
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        const value = req.body.queryResult.queryText;
+        const query = recentLog.query;
+        const illness = await Illness.find({ title: query });
 
-            console.log("Intent Name: " + intent_name);
-            console.log("Query Text: " + value);
-            console.log("Recent Log Query: " + query);
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
 
         res.json({
             "fulfillmentMessages": [
@@ -169,98 +167,98 @@ async function _Illness(req, res) {
             query: value,
             isAnswered: false,
         });
-        await log4.save();   
-}
-else if (intent_name == 'Illness - more - treatment') {
-    const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-    const value = req.body.queryResult.queryText;
-    const query = recentLog.query;
-    const illness = await Illness.find({title: query});
+        await log4.save();
+    }
+    else if (intent_name == 'Illness - more - treatment') {
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        const value = req.body.queryResult.queryText;
+        const query = recentLog.query;
+        const illness = await Illness.find({ title: query });
 
-    console.log("Intent Name: " + intent_name);
-    console.log("Query Text: " + value);
-    console.log("Recent Log Query: " + query);
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
 
-    res.json({
-        "fulfillmentMessages": [
-            {
-                "quickReplies": {
-                    "title": illness[0].treatment,
-                    "quickReplies": [
-                        "Details",
-                        "Symptoms",
-                        "Treatment",
-                        "Prevention",
-                        "Go Back"
-                    ]
+        res.json({
+            "fulfillmentMessages": [
+                {
+                    "quickReplies": {
+                        "title": illness[0].treatment,
+                        "quickReplies": [
+                            "Details",
+                            "Symptoms",
+                            "Treatment",
+                            "Prevention",
+                            "Go Back"
+                        ]
+                    },
+                    "platform": "FACEBOOK"
                 },
-                "platform": "FACEBOOK"
-            },
-            {
-                "text": {
-                    "text": [
-                        ""
-                        // result
-                    ]
+                {
+                    "text": {
+                        "text": [
+                            ""
+                            // result
+                        ]
+                    }
                 }
-            }
-        ]
-    });
-    const log5 = new LogQuery({
-        query: value,
-        isAnswered: false,
-    });
-    await log5.save();   
-}
-else if (intent_name == 'Illness - more - prevention') {
-    const recentLog = await LogQuery.findOne({isAnswered:true}).sort({datePosted: -1});
-    const value = req.body.queryResult.queryText;
-    const query = recentLog.query;
-    const illness = await Illness.find({title: query});
+            ]
+        });
+        const log5 = new LogQuery({
+            query: value,
+            isAnswered: false,
+        });
+        await log5.save();
+    }
+    else if (intent_name == 'Illness - more - prevention') {
+        const recentLog = await LogQuery.findOne({ isAnswered: true }).sort({ datePosted: -1 });
+        const value = req.body.queryResult.queryText;
+        const query = recentLog.query;
+        const illness = await Illness.find({ title: query });
 
-    console.log("Intent Name: " + intent_name);
-    console.log("Query Text: " + value);
-    console.log("Recent Log Query: " + query);
+        console.log("Intent Name: " + intent_name);
+        console.log("Query Text: " + value);
+        console.log("Recent Log Query: " + query);
 
-    res.json({
-        "fulfillmentMessages": [
-            {
-                "quickReplies": {
-                    "title": illness[0].prevention,
-                    "quickReplies": [
-                        "Details",
-                        "Symptoms",
-                        "Treatment",
-                        "Prevention",
-                        "Go Back"
-                    ]
+        res.json({
+            "fulfillmentMessages": [
+                {
+                    "quickReplies": {
+                        "title": illness[0].prevention,
+                        "quickReplies": [
+                            "Details",
+                            "Symptoms",
+                            "Treatment",
+                            "Prevention",
+                            "Go Back"
+                        ]
+                    },
+                    "platform": "FACEBOOK"
                 },
-                "platform": "FACEBOOK"
-            },
-            {
-                "text": {
-                    "text": [
-                        ""
-                        // result
-                    ]
+                {
+                    "text": {
+                        "text": [
+                            ""
+                            // result
+                        ]
+                    }
                 }
-            }
-        ]
-    });
-    const log6 = new LogQuery({
-        query: value,
-        isAnswered: false,
-    });
-    await log6.save();   
-}
+            ]
+        });
+        const log6 = new LogQuery({
+            query: value,
+            isAnswered: false,
+        });
+        await log6.save();
+    }
 
-    
+
     else {
         const log = new LogQuery({
             query: value,
             isAnswered: false,
         });
-        await log.save();   
+        await log.save();
     }
 }
 
