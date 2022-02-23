c62a08eaf2987dbaab6aeeae4de31408137a9cd5
unknown <louiejaysalvana@gmail.com>
2022-02-23 20:45:23 +0800

    update

---
+++ controllers/intents/DefaultWelcomeIntent.js
@@ -0,0 +1,15 @@
+'use strict';
+
+async function _DefaultWelcomeIntent(req, res) {
+    let intent_name = req.body.queryResult.intent.displayName;
+    const value = req.body.queryResult.queryText;
+
+     if (intent_name == "Default Welcome Intent") {
+        const log3 = new EventLogQuery({
+            query: value,
+            isAnswered: true
+        });
+        console.log(value);
+    }
+}
+module.exports = _DefaultWelcomeIntent;
