838516645176b4670a237d43fb12b70edc7d6631
unknown <louiejaysalvana@gmail.com>
2022-02-24 19:15:48 +0800

    update

--- controllers/intents/DefaultWelcomeIntent.js
+++ controllers/intents/DefaultWelcomeIntent.js
@@ -1,25 +1,25 @@
-// 'use strict';
+'use strict';
 
-// const mongoose = require('mongoose');
-// const DefLogQuery = require('../../models/Logs');
+const mongoose = require('mongoose');
+const DefLogQuery = require('../../models/Logs');
 
-// async function _DefaultWelcomeIntent(req, res) {
-//     let intent_name = req.body.queryResult.intent.displayName;
-//     const value = req.body.queryResult.queryText;
+async function _DefaultWelcomeIntent(req, res) {
+    let intent_name = req.body.queryResult.intent.displayName;
+    const value = req.body.queryResult.queryText;
 
-//     if (intent_name == "Default Welcome Intent") {
-//         const log3 = new DefLogQuery({
-//             query: value,
-//             isAnswered: true
-//         });
-//         console.log(value);
-//     }
-//     // else {
-//     //     const log = new DefLogQuery({
-//     //         query: value,
-//     //         isAnswered: false
-//     //     });
-//     //     await log.save();
-//     // }
-// }
-// module.exports = _DefaultWelcomeIntent; 
+    if (intent_name == "Default Welcome Intent") {
+        const log3 = new DefLogQuery({
+            query: value,
+            isAnswered: true
+        });
+        console.log(value);
+    }
+    // else {
+    //     const log = new DefLogQuery({
+    //         query: value,
+    //         isAnswered: false
+    //     });
+    //     await log.save();
+    // }
+}
+module.exports = _DefaultWelcomeIntent; 
