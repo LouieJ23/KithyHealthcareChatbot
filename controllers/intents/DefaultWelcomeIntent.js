9b57575c2d347cd9365cca1431c60ff8a83f9363
unknown <louiejaysalvana@gmail.com>
2022-02-24 17:33:43 +0800

    update

--- controllers/intents/DefaultWelcomeIntent.js
+++ controllers/intents/DefaultWelcomeIntent.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const mongoose = require('mongoose');
 const DefLogQuery = require('../../models/Logs');
 
 async function _DefaultWelcomeIntent(req, res) {
     let intent_name = req.body.queryResult.intent.displayName;
     const value = req.body.queryResult.queryText;
 
-     if (intent_name == "Default Welcome Intent") {
+    if (intent_name == "Default Welcome Intent") {
         const log3 = new DefLogQuery({
             query: value,
             isAnswered: true
