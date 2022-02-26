3626569d3abb3bbd96f93398dd8fe5ea8237f291
unknown <louiejaysalvana@gmail.com>
2022-02-26 23:00:16 +0800

    2/26/22

--- controllers/intents/EventIntent.js
+++ controllers/intents/EventIntent.js
@@ -748,7 +748,7 @@ async function _Event(req, res) {
     else if (intent_name == "Events - upcoming") {
         Event.find({}, function (err, events) {
             const event = events[0];
-            let eventDate = event.datePosted;
+            let eventDate = Date.parse(event.datePosted);
             let dateToday = Date.now();
 
             var result = "The upcoming event " + event.eventTitle + " will be going to held  in " + event.eventLocation + ". So in order to participate to the event, you are required to bring " + event.eventRequire + ". The process is to " + event.eventProcess + " and the participants are " + event.eventParticipant + ".";
