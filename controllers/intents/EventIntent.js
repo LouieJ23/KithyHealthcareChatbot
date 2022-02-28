15577df25f9e1ad7fd4e8ca853357a19ca75631d
unknown <louiejaysalvana@gmail.com>
2022-02-28 22:54:53 +0800

    update

--- controllers/intents/EventIntent.js
+++ controllers/intents/EventIntent.js
@@ -710,7 +710,7 @@ async function _Event(req, res) {
         console.log(upcomingEvents);
         console.log(intent_name);
 
-        if (upcomingEvents === null) {
+        if (upcomingEvents != null) {
             res.json({
                 "fulfillmentMessages": [
                     {
