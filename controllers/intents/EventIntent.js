d664ae089b03619d724bad2f1108f82783bc5d2c
unknown <louiejaysalvana@gmail.com>
2022-05-19 00:29:01 +0800

    previousEvent

--- controllers/intents/EventIntent.js
+++ controllers/intents/EventIntent.js
@@ -717,8 +717,8 @@ async function _Event(req, res) {
         for (let i = 0; i < events.length; i++) {
             var startDate = events[i].startDate.getTime();
             if (startDate < currentDate) {
-                // result += "The previous event is " + events[i].eventTitle + ". " + events[i].eventDetails + ". This event took place in " + events[i].eventLocation + ", started on " + events[i].startDate.toString().slice(0,15) + " at " + events[i].timeStart + " until " + events[i].endDate.toString().slice(0,15) + " at " + events[i].timeEnds + ". To take part in this event, you were required to meet the following requirements: " + events[i].eventRequire + ". To take part in this event, you must complete the steps below:  " + events[i].eventProcess + ". The participants for this event were " + events[i].eventParticipant + ".\n" + "\n";
-                result += "The previous event date are " + events[i].startDate + "\n";
+                result += "The previous event is " + events[i].eventTitle + ". " + events[i].eventDetails + ". This event took place in " + events[i].eventLocation + ", started on " + events[i].startDate.toString().slice(0,15) + " at " + events[i].timeStart + " until " + events[i].endDate.toString().slice(0,15) + " at " + events[i].timeEnds + ". To take part in this event, you were required to meet the following requirements: " + events[i].eventRequire + ". To take part in this event, you must complete the steps below:  " + events[i].eventProcess + ". The participants for this event were " + events[i].eventParticipant + ".\n" + "\n";
+                // result += "The previous event date are " + events[i].startDate + "\n";
                 count++;
             }
         }
