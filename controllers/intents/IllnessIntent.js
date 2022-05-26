b9af51b314e52242de04d1902f0d491a2d693a4f
unknown <louiejaysalvana@gmail.com>
2022-05-26 19:39:32 +0800

    Update

--- controllers/intents/IllnessIntent.js
+++ controllers/intents/IllnessIntent.js
@@ -40,11 +40,7 @@ async function _Illness(req, res) {
                 }
             ]
         });
-        const log1 = new queryLog({
-            query: value,
-            isAnswered: true,
-        });
-        await log1.save();
+        
     }
     else if (intent_name == 'Illness - more') {
         const recentLog = await queryLog.findOne({ isAnswered: true }).sort({ datePosted: -1 });
@@ -76,12 +72,6 @@ async function _Illness(req, res) {
                 }
             ]
         });
-
-        const log2 = new queryLog({
-            query: value,
-            isAnswered: true,
-        });
-        await log2.save();
 
     }
     else if (intent_name == 'Illness - more - details') {
